2dadeb5492ce0eb3f293298747a499edf64ed909
Soham Parekh <soham.parekh2010@gmail.com>
2020-10-27 01:48:30 +0400

    Added some employee pages.

--- gui.py
+++ gui.py
@@ -233,12 +233,6 @@ def check_profit():
     else:
         print("Error! cannot create the database connection.")
 
-
-
-
-
-
-
 class StartPage:
     def __init__(self, window):
         def callback(event):
@@ -250,7 +244,7 @@ class StartPage:
             elif 463<= event.x<=751 and 385 <= event.y<=436:
                 print("employee")
                 C.destroy()
-                EmpPage(window)
+                EmployeeLoginPage(window)
 
         C = Canvas(window, height=756, width=1210)
         C.bind("<Button-1>", callback)
@@ -299,106 +293,91 @@ class AdminLoginPage:
         D.pack()
         window.mainloop()
 
-class LoadPage:
+class EmployeeLoginPage:
     def __init__(self, window):
-        D = Canvas(window, height=608, width=400)
+        D = Canvas(window, height=756, width=1210)
+
+        def validate_login():
+            emp_id = password_entry.get()
+            D.destroy()
+            password_entry.destroy()
+            login_button.destroy()
+            print(emp_id)
+            EmployeePage(window,emp_id)
 
         def callback(event):
             global flag
-            if 107 <= event.x <= 293 and 207 <= event.y <= 269:
-                D.destroy()
-                print("Starting game......")
-                global players, numbers, role, names, player
-                fptr = open("info.txt", "r")
-                f1 = fptr.readlines()
-                players_number = int(f1[0])
-                ind = 1
-                for _ in range(players_number):
-                    names.append(f1[ind].strip("\n"))
-                    ind += 1
-                for _ in range(players_number):
-                    numbers.append(f1[ind].strip("\n"))
-                    ind += 1
-                v = int(f1[ind].strip("\n"))
-                ind += 1
-                s = int(f1[ind].strip("\n"))
-                count = 0
-                for _ in range(v):
-                    role.append("virus")
-                for _ in range(s):
-                    role.append("service")
-                random.shuffle(role)
-                # for a crazy game:
-                # jobs = [0, 0, 0, 0, 0, 0]
-                if players_number > 5:
-                    jobs = [0, 1, 2, 3, 4, 5]
-                else:
-                    jobs = [1, 2, 3, 4, 5]
-                hidden = [6, 7, 8, 9, 10]
-                random.shuffle(jobs)
-                while jobs[0] == 3 or jobs[1] == 3:
-                    random.shuffle(jobs)
-                random.shuffle(hidden)
-                index = 0
-                extra = 0
-                # print(players_number)
-                # print(role)
-                for _ in range(players_number):
-                    name = names.pop()
-                    number = numbers.pop()
-                    if _ > 5:
-                        job = jobs[extra]
-                        extra += 1
-                    else:
-                        job = jobs[_]
-                    if job == 0:
-                        job = hidden[0]
-                        random.shuffle(hidden)
-                    players.append(player(name, number, role.pop(), job))
-                    index += 1
-                random.shuffle(players)
-                viruses = []
-                for player in players:
-                    if player.role == "virus":
-                        viruses.append(player)
-                for player in players:
-                    if player.role == "service":
-                        message = (
-                            "Welcome to Deception!\n You are working for "
-                            + player.role
-                            + ".\n Your mission is to find all the V.I.R.U.S Agents."
-                        )
-                        send_info(player.number, message)
-                        print(player.name,player.role,file=open("stats.txt","a+"))
-                    else:
-                        message = (
-                            "Welcome to Deception!\n You are working for "
-                            + player.role
-                            + ".\n Your team is:\n"
-                        )
-                        for v in viruses:
-                            message += v.name
-                            message += "\n"
-                        message += "All the best!"
-                        send_info(player.number, message)
-                        print(player.name,player.role,file=open("stats.txt","a+"))
-                DATABASE_URL = 'postgres://xtnoshkjrfaipj:609d5dfaf6c3af5a7fd16a87c2fec1591c27eab51cc93a61126b875bc5e3e8b5@ec2-34-192-122-0.compute-1.amazonaws.com:5432/der5779vnluga7'
-                conn = psycopg2.connect(DATABASE_URL, sslmode='require')
-                cur=conn.cursor()
-                cur.execute("""DROP TABLE IF EXISTS votes""")
-                cur.execute("""CREATE TABLE votes (
-                            vote VARCHAR(255) NOT NULL
-                        )""")
-                cur.close()
-                conn.commit()
-                conn.close()
-                GamePage(window)
-
+            print(event.x,event.y)
+        helv36 = tkFont.Font(family='Helvetica', size=15, weight='bold')
         D.bind("<Button-1>", callback)
-        background = PhotoImage(file="images/names.png")
+        background = PhotoImage(file="images/employeelogin.png")
         D.create_image(0, 0, image=background, anchor="nw")
+        password= StringVar()
+        password_entry = Entry(window,textvariable=password,width=30)
+        password_entry['font']=helv36
+        password_entry.pack()
+        password_entry.place(x=460,y=269)
+        login_button = Button(window,text="LOGIN",command = validate_login)
+        login_button['font']=helv36
+        login_button.pack()
+        login_button.place(x=573,y=335)
         D.pack()
         window.mainloop()
+
+class EmployeePage:
+    def __init__(self, window,emp_id):
+        def callback(event):
+            global flag
+            print(event.x,event.y)
+            if 173<=event.x<=503 and 180<=event.y<=225:
+                C.destroy()
+                EmployeeReservationPage(window,emp_id)
+            elif 256<=event.x<=433 and 312<=event.y<=368:
+                C.destroy()
+                EmployeeBillingPage(window,emp_id)
+            elif 158<=event.x<=525 and 450<=event.y<=496:
+                C.destroy()
+                EmployeeCheckPage(window,emp_id,1)
+                #author
+            elif 704<=event.x<=1036 and 180<=event.y<=223:
+                C.destroy()
+                EmployeeReturnPage(window,emp_id)
+            elif 706<=event.x<=1025 and 356<=event.y<=314:
+                C.destroy()
+                EmployeeCheckPage(window,emp_id,2)
+                #book
+            elif 738<=event.x<=996 and 449<=event.y<=495:
+                C.destroy()
+                EmployeeCheckPage(window,emp_id,3)
+                #CD
+
+
+        C = Canvas(window, height=756, width=1210)
+        C.bind("<Button-1>", callback)
+        background_image = PhotoImage(file="images/employeemain.png")
+        C.create_image(0, 0, image=background_image, anchor="nw")
+        window.title("PJ Store")
+        C.pack()
+        window.mainloop()
+
+class EmployeeReservationPage:
+    def __init__(self, window,emp_id):
+        def callback(event):
+            global flag
+            print(event.x,event.y)
+            if 40<=event.x<=210 and 616<=event.y<=664:
+                C.destroy()
+                EmployeePage(window,emp_id)
+
+        #place entry box at (412,427) and (439,259)
+        C = Canvas(window, height=756, width=1210)
+        C.bind("<Button-1>", callback)
+        background_image = PhotoImage(file="images/employeereserve.png")
+        C.create_image(0, 0, image=background_image, anchor="nw")
+        window.title("PJ Store")
+        C.pack()
+        window.mainloop()
+
 
 class AdminPage:
     def __init__(self, window):
